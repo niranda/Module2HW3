ff5f0d5d11179f7832798b2e89b787c862263ef0
niranda <teslatorgames@gmail.com>
2021-11-18 20:42:55 +0200

    added finished project

---
+++ ShopApp/ShopApp/Providers/GiftProvider.cs
@@ -0,0 +1,18 @@
+﻿using ShopApp.Models;
+using ShopApp.Services;
+namespace ShopApp.Providers
+{
+    public class GiftProvider
+    {
+        private readonly ConfigService _configuration;
+
+        public GiftProvider()
+        {
+            _configuration = ConfigService.Instance;
+            CandiesContainer = new Sweet[_configuration.AllowedContainerCapacity];
+        }
+
+        public Sweet[] CandiesContainer { get; set; }
+        public double GeneralWeight { get; set; }
+    }
+}
